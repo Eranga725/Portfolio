b4082a8b2727e63eb4e1c805d62c230959496f79
Eranga727 <hansanaeranga2001@gmail.com>
2025-12-11 22:12:34 +0500

    Fix: install gsap and locomotive-scroll

--- app/components/Landing/index.jsx
+++ app/components/Landing/index.jsx
@@ -3,69 +3,65 @@ import styles from './style.module.css'
 import Image from 'next/image'
 import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'    
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export default function Landing() {
     const firstText = useRef(null);
     const secondText = useRef(null);
     const slider = useRef(null);
+
     let xPercent = 0;
     let direction = -1;
 
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
 
+        gsap.to(slider.current, {
+            scrollTrigger: {
+                trigger: document.documentElement,
+                scrub: 0.5,
+                start: 0,
+                end: window.innerHeight,
+                onUpdate: (e) => (direction = e.direction * -1),
+            },
+            x: "-500px",
+        });
 
-  useEffect( () => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.5,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
-      },
-      x: "-500px",
-    })
-    requestAnimationFrame(animate);
-  }, [])
+        requestAnimationFrame(animate);
+    }, []);
 
+    const animate = () => {
+        if (xPercent < -100) xPercent = 0;
+        else if (xPercent > 0) xPercent = -100;
 
+        gsap.set(firstText.current, { xPercent });
+        gsap.set(secondText.current, { xPercent });
 
-  const animate = () => {
-    if(xPercent < -100){
-      xPercent = 0;
-    }
+        xPercent += 0.1 * direction;
+        requestAnimationFrame(animate);
+    };
 
-    else if(xPercent > 0){
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, {xPercent: xPercent})
-    gsap.set(secondText.current, {xPercent: xPercent})
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
     return (
-    <main className={styles.main}>
-        <Image 
-        src="/images/background.jpg"
-        fill={true}
-        alt="background"
-      />
+        <main className={styles.main}>
+            <Image src="/images/background.jpg" fill alt="background" />
 
-      <div className={styles.sliderContainer}>
-        <div ref={slider} className={styles.slider}>
-          <p ref={firstText}>UI/UX Developer - </p>
-          <p ref={secondText}>UI/UX Developer - </p>
-        </div>
-      </div>
-      <div data-scroll data-scroll-speed={0.1} className={styles.description}>
-        <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
-        </svg>
-        <p>Freelance</p>
-        <p>Designer & Developer</p>
-      </div>
-    </main>
-  )
+            <div className={styles.sliderContainer}>
+                <div ref={slider} className={styles.slider}>
+                    <p ref={firstText}>UI/UX Developer - </p>
+                    <p ref={secondText}>UI/UX Developer - </p>
+                </div>
+            </div>
+
+            <div data-scroll data-scroll-speed={0.1} className={styles.description}>
+                <svg width="9" height="9" viewBox="0 0 9 9" fill="none">
+                    <path
+                        d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
+                        fill="white"
+                    />
+                </svg>
+                <p>Freelance</p>
+                <p>Designer & Developer</p>
+            </div>
+        </main>
+    );
 }
-

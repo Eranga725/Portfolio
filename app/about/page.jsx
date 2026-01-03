432ccb9c0751d6b825c23740689724167f6ed9d9
Eranga727 <hansanaeranga2001@gmail.com>
2026-01-03 16:47:40 +0530

    update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -24,7 +24,6 @@ export default function WorkPage() {
       <Grid />
       <NEXT />
       <Contact />
-
       </div>
     </main>
   )
--- app/components/Header/nav/index.jsx
+++ app/components/Header/nav/index.jsx
@@ -20,7 +20,7 @@ const navItems = [
   },
   {
     title: "About",
-    href: "#about",
+    href: "/about",
   },
   {
     title: "Contact",
